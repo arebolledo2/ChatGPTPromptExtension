18ebf48de2ad39cc93ebd1c92a4077c471cbfd21
Andres Rebolledo <andres@rtbsoftware.com>
2023-05-22 07:53:30 -0600

    Add project files.

---
+++ ChatGPTPrompt/Commands/PopulateCodeToTestCommand.cs
@@ -0,0 +1,120 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace ChatGPTPrompt.Commands
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class PopulateCodeToTestCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 256;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("e4d82029-ec8e-4f62-84b0-ef59fe7501ab");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PopulateCodeToTestCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private PopulateCodeToTestCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static PopulateCodeToTestCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in PopulateCodeToTestCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new PopulateCodeToTestCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private async void Execute(object sender, EventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            // Get the DTE object, which represents the Visual Studio IDE.
+            var dte = await ServiceProvider.GetServiceAsync<DTE, DTE2>();
+            
+            // Get the selected text.
+            var selectedText = dte.ActiveDocument.Selection as TextSelection;
+
+            // If no text is selected, do nothing.
+            if (selectedText == null || selectedText.IsEmpty)
+            {
+                return;
+            }
+
+            // Find your tool window.
+            // You'll need to replace `YourPackageClass` with the type of your package class,
+            // and `YourToolWindow` with the type of your tool window class.
+            var window = await package.FindToolWindowAsync(typeof(PromptSelectionToolWindow), 0, true, package.DisposalToken) as PromptSelectionToolWindow;
+
+            // Check if the window was found.
+            if (window?.Content is PromptSelectionToolWindowControl control)
+            {
+                // If it was, set the text of the codeToTestTextBox.
+                control.codeToTestTextBox.Text = selectedText.Text;
+            }
+        }
+    }
+}
